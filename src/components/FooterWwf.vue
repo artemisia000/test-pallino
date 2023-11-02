0af4a0807e054cb7ea1b24b1b8bbf5019cfa8ff5
Misia Pierobon <misiapierobon@gmail.com>
2023-11-02 22:35:59 +0100

    footer

--- src/components/FooterWwf.vue
+++ src/components/FooterWwf.vue
@@ -1,9 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-   
-    <h3>footer</h3>
-  
+  <div class="row w-footer">
+    <div class="col-12 col-md-4 col-lg-3 pe-0 ps-0">
+      <div class="w-box-footer">
+        <h1>LOREM IPSUM</h1>
+        <ul>
+          <li>Via decorati al valor civile, 57</li>
+          <li>P.Iva: IT 02221111115</li>
+          <li>C.F: 80078432222</li>
+          <li>Telefono: 01444324</li>
+          <li> E-mail: email@email.it - test@test.it</li>
+          <li> Sito ufficiale: pallino.it</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-12 col-md-4 col-lg-3 pe-0 ps-0">
+      <div class="w-box-footer">
+        <ul>
+          <li>Voice 1 </li>
+          <li>Voice 2 </li>
+          <li>Lorem ipsum </li>
+          <li>Dolor sit </li>
+          <li>Voice 3</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-12 col-md-4 col-lg-3 pe-0 ps-0">
+      <div class="w-box-footer">
+        <ul>
+          <li>Domande frequenti</li>
+          <li>Privacy policy</li>
+          <li>Termini e condizioni </li>
+          <li>Telefono: 01444324</li>
+          <li>Centro preferenze cookies</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-12 col-md-4 col-lg-3 pe-0 ps-0">
+      <div class="w-box-footer">
+        <ul>
+          <li><img src="../assets/fb.svg"/>Facebook</li>
+          <li><img src="../assets/is.svg"/>Instagram</li>
+          <li><img src="../assets/tw.svg"/>Twitter</li>
+          <li><img src="../assets/yt.svg"/>You Tube</li>
+        </ul>
+      </div>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -18,18 +62,48 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+@import '@/styles/variables';
+
+.w-footer{
+  background-color: $grey-secondary-color;
+  height: 440px;
+  padding-bottom: 80px;
+
+  h1{
+    color:  $grey-text-color;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 40px;
+    text-transform: uppercase;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.w-box-footer{
+  padding: 20px;
+  text-align: left;
+  height: 378px;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  border-bottom: 1px solid $grey-text-color;
+  &:nth-child(1),
+  &:nth-child(2){
+    border-right: 1px solid $grey-text-color;
+  }
+  
+  ul{
+    padding-left: 0px;
+  }
+
+  ul li{
+    color: $grey-text-color;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    list-style: none;
+
+  }
 }
 </style>
